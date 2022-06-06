c37b12effe2d1110283efb2220197c5c527388b5
Alex Bogdanovich <7894664@mail.ru>
2022-06-06 20:08:47 +0300

    add create task controller

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { Task } from './task.models';
+import { stringify } from 'querystring';
+import { Task, TaskStatus } from './task.models';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class TasksService {
@@ -7,5 +9,17 @@ export class TasksService {
 
   getAllTask(): Task[] {
     return this.tasks;
+  }
+
+  createTask(title: string, description: string): Task {
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+
+    this.tasks.push(task);
+    return task;
   }
 }
